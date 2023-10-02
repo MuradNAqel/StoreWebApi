175a130d3426736f8ed5a04d00a67dd056551bec
MuradNAqel <muradaqel.98@google.com>
2023-10-02 08:50:07 +0300

    Added pizza service and pizza controllerAdd project files.

---
+++ StoreWebApi/models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace StoreWebApi.models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
